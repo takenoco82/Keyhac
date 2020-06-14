6014d055b5844419fd217fa3b223406958b920fb
Daisuke TAKEUCHI <takenoco82@gmail.com>
2020-06-14 10:59:41 +0900

    [fix] ファイル名を変更

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 readonly PROJECT_DIR=$(git rev-parse --show-toplevel)
 
-ln -sfv ${PROJECT_DIR}/mac/config.py ~/Library/Application\ Support/Keyhac/config.py
+ln -sfv ${PROJECT_DIR}/config_mac.py ~/Library/Application\ Support/Keyhac/config.py
